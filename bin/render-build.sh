ad54bec57eec2ed5cf5c2e926eaf04622cd0832f
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-01-26 18:02:23 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,5 @@ bundle install
 yarn install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:create
-bundle exec rake db:migrate
+bundle exec rake db:migrate --trace
 bundle exec rake db:seed
